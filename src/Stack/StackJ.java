028bb454752533921a7bc8afa988c6c2e0fe376b
ali amer <76897266+joeamerson@users.noreply.github.com>
2021-05-17 11:43:21 +0300

    new update

--- src/Stack/StackJ.java
+++ src/Stack/StackJ.java
@@ -4,56 +4,54 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class StackJ {
-    private int size ;
+    private int size;
     private int[] items = new int[5];
-    public void push(int item){
+
+    public void push(int item) {
         if (size == items.length)
             throw new StackOverflowError();
         items[size++] = item;
     }
-    public int pop (){
-        if (size== 0)
+
+    public int pop() {
+        if (size == 0)
             throw new IllegalStateException();
 
         return items[--size];
     }
-    public int peek(){
+
+    public int peek() {
         if (size == 0)
             throw new IllegalStateException();
         return items[size - 1];
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return size == 0;
     }
+
     @Override
-    public String toString(){
-        var contain = Arrays.copyOfRange(items,0 , size);
+    public String toString() {
+        var contain = Arrays.copyOfRange(items, 0, size);
         return Arrays.toString(contain);
     }
+
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        int top=0 ;
-        int index= 0;
-        Stack<Integer> stack = new Stack<Integer>();
-         if (top  < popped.length && pushed.length == popped.length){
-             while (index+1 < popped.length){
-                 int popValue = popped[index];
-                 if(popValue != pushed[top] && top+1 < pushed.length){
-                     stack.push(pushed[top]);
-                         top++;
-                 } else if (popped[index]== stack.pop()){
-                         stack.pop();
-                         top--;
-                 }
-                 else
-                     index++;
-                 }
-
-             }
-
-        return stack.isEmpty();
+        int[] stack = new int[pushed.length];
+        int top = -1;
+        int pop = 0;
+        for(int i = 0; i < pushed.length; i++) {
+            stack[++top] = pushed[i];
+            while(popNotBigAndTopNotSmallAndItemExist(popped,stack, top, pop)) {
+                top--;
+                pop++;
+            }
         }
-
-
-
+        return top == -1;
     }
 
+    private boolean popNotBigAndTopNotSmallAndItemExist(int[] popped, int[] stack, int top, int pop ) {
+
+        return pop < popped.length && top >= 0 && stack[top] == popped[pop];
+    }
+}
